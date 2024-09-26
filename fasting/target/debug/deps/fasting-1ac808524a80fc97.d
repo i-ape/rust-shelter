512a9256233b6ee9cd459245ffe19190e3d59f8c
i-ape <i-ape@proton.me>
2024-09-26 20:44:59 +0200

    rust init and label

---
+++ fasting/target/debug/deps/fasting-1ac808524a80fc97.d
@@ -0,0 +1,9 @@
+/home/eat/code/projects/rust-shelter/fasting/target/debug/deps/libfasting-1ac808524a80fc97.rmeta: src/main.rs Cargo.toml
+
+/home/eat/code/projects/rust-shelter/fasting/target/debug/deps/fasting-1ac808524a80fc97.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ fasting/target/debug/deps/fasting-f93ab6c0f97fda5f.d
@@ -0,0 +1,9 @@
+/home/eat/code/projects/rust-shelter/fasting/target/debug/deps/libfasting-f93ab6c0f97fda5f.rmeta: src/main.rs Cargo.toml
+
+/home/eat/code/projects/rust-shelter/fasting/target/debug/deps/fasting-f93ab6c0f97fda5f.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
